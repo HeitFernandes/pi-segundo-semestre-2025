498b7159c811bbabdb74ffc98114d47aac0c1fb5
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-10-02 01:03:34 -0300

    arquitetura de pastas completa

---
+++ projeto-oficina/backend/config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db_name = '';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+}
+
+?>
---
+++ projeto-oficina/frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'plugin:react/recommended', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0, // Necessidade de criar um arquivo .jsx
+    'import/prefer-default-export': 0, // Todos os exports precisam conter default, nesse projeto, alguns não terão
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn', // Regras do próprio REACT HOOKS
+    'import/no-extraneous-dependencies': 0,
+    'no-console': 0,
+    'default-param-last': 0,
+  },
+};
