472fe844f53110afe2e443a5df13f029c30c4d39
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-11-09 12:20:37 -0300

    configurações do deploy no frontend/backend

--- projeto-oficina/backend/config/db.php
+++ projeto-oficina/backend/config/db.php
@@ -1,4 +1,21 @@
 <?php
+$allowedOrigins = [
+    'http://localhost:3000',
+    'https://thankful-river-054543d0f.3.azurestaticapps.net'
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 $isAzureEnvironment = getenv('WEBSITE_SITE_NAME');
 
 if ($isAzureEnvironment) {
