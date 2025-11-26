445ca07915fc713ec911943d32aecf3206488b67
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-11-26 09:50:03 -0300

    feature/set status do agendamento como cancelado

--- projeto-oficina/backend/agendamentos/delete.php
+++ projeto-oficina/backend/agendamentos/delete.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
