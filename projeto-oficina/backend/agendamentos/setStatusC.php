b258df9021f00b92906f10cd3f627e8343c3a5d2
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-11-25 22:03:40 -0300

    feature/cadastro de agendamentos e visualização de agendamentos completos.

---
+++ projeto-oficina/backend/agendamentos/setStatusC.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once '../config/db.php';
+
+?>
