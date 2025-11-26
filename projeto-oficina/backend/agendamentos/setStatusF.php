de8b005454593650217180229fb08634ee2f5f21
1freelipe <feliperodrigues_spfc@hotmail.com>
2025-11-26 09:59:31 -0300

    feature/set status finalizado nos agendamentos

--- projeto-oficina/backend/agendamentos/setStatusF.php
+++ projeto-oficina/backend/agendamentos/setStatusF.php
@@ -1,4 +1,42 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, UPDATE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+require_once '../config/db.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+$id_agendamento = $_GET['id'];
+
+if(!$id_agendamento) {
+    http_response_code(404);
+    echo json_encode(['success' => false, 'message' => 'ID não fornecido.']);
+    exit;
+}
+
+$novo_status = 'F';
+
+try {
+    $stmt = $pdo->prepare('UPDATE tb_agendamento_de_servico
+    SET AGEN_STATUS = ?');
+    $stmt->execute([$novo_status]);
+
+    if($stmt->rowCount() > 0) {
+        http_response_code(200);
+        echo json_encode(['success' => true, 'message' => 'Agendamento finalizado.']);
+    } else {
+        http_response_code(200);
+        echo json_encode(['succes' => true, 'message' => 'Falha ao finalizar o agendamento.']);
+    }
+} catch(PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['success' => false, 'message' => 'Ocorreu um erro ao tentar se conectar com o servidor: ' . $e->getMessage()]);
+}
 
 ?>
